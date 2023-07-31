2c9ff68854832e5c79c24f3257267aa1cc817e7f
meganath2013 <meganath2013@gmail.com>
2023-07-31 00:01:26 -0700

    api fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state=
+    {monsters:[]
+    };
+  }
+
+  render(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then( data => 
+      this.setState(
+        ()=>{
+          return {monsters:data};
+        },
+        ()=>{
+          console.log(this.state.monsters)
+        }
+      )
+      )
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      {
+        this.state.monsters.map((data)=> 
+        {  
+          return(      
+          <div key={data.id}>
+          <h1> {data.name} </h1>
+          </div>)
+        }
+        )
+
+      }
+      </div>
+      );
+    }
+  }
 
 export default App;
